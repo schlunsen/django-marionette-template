b2c306bb293cd3940be35f1cbc14fd7f71a9c933
Rasmus Schlünsen <rs@konstellation.dk>
2013-04-28 20:29:15 +0200

    Removed pipline

--- django/project_name/project_name/settings.py
+++ django/project_name/project_name/settings.py
@@ -123,7 +123,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'pipeline',
     'django_extensions',
     '{{ project_name }}.main',
 )
