94d522b34c747815ba7471d98550867ad217852f
Rasmus Schlünsen <rs@konstellation.dk>
2013-04-28 20:57:52 +0200

    Fixed template escapes

--- django/project_name/project_name/settings.py
+++ django/project_name/project_name/settings.py
@@ -105,10 +105,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'project_name.urls'
+ROOT_URLCONF = '{{project_name}}.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'project_name.wsgi.application'
+WSGI_APPLICATION = '{{project_name}}.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -125,6 +125,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.admin',
     'django_extensions',
+    'south',
     '{{ project_name }}.main',
 )
 
